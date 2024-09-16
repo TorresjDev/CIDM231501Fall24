1c41357dce5b3bd5517da06630be8ea88471b97d
TorresjDev <torres.jesus2143@gmail.com>
2024-09-16 10:42:51 -0500

    hw3 due todaygit add .

--- Homework/Homework3/Program.cs
+++ Homework/Homework3/Program.cs
@@ -5,7 +5,10 @@ class Program
     static void Main(string[] args)
     {
         //* Code for Q1
-
+        Console.WriteLine("Is N a Prime Number");
+        Console.WriteLine("Input an integer: ");
+        int N = Convert.ToInt16(Console.ReadLine());
+        if ()
 
 
 
