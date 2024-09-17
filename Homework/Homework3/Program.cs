17e250b038ff6a07961752d2978d14abff565542
TorresjDev <j.torres3.dev@gmail.com>
2024-09-16 20:19:32 -0500

    hw 3 completed

--- Homework/Homework3/Program.cs
+++ Homework/Homework3/Program.cs
@@ -8,12 +8,64 @@ class Program
         Console.WriteLine("Is N a Prime Number");
         Console.WriteLine("Input an integer: ");
         int N = Convert.ToInt16(Console.ReadLine());
-        if ()
+        bool isPrime = true;
+        if (N <= 1)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 2; i < N; i++)
+            {
+                if (N % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
 
+        if (isPrime)
+        {
+            Console.WriteLine($"Yes! {N} is a prime number");
+        }
+        else
+        {
+            Console.WriteLine($"No, {N} is not a prime number");
+        }
+        Console.WriteLine("End of Q1");
 
 
         //* Code for Q2
+        Console.WriteLine("Print # amounts");
+        Console.WriteLine("Input an integer: ");
+        int num = Convert.ToInt16(Console.ReadLine());
+
+        for (int i = 0; i < num; i++)
+        {
+            for (int j = 0; j < num; j++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("End of Q2");
+
         //* Code for BQ
+        Console.WriteLine("Diagonal Pattern");
+        int num1 = Convert.ToInt16(Console.ReadLine());
+
+        for (int i = 1; i <= num1; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("End of BQ");
 
     }
 }
