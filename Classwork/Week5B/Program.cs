e52490f77a3e3705a7b5ce23735246c7012645a3
TorresjDev <torres.jesus2143@gmail.com>
2024-09-23 10:26:08 -0500

    week 5-1

---
+++ Classwork/Week5B/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Week5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Week 5 nice!");
+        // Console.WriteLine("Please input 2 integers");
+        // int int1 = getInteger();
+        // int int2 = getInteger();
+        // int sum = TwoSum(int1, int2);
+        // Console.WriteLine($"{int1} + {int2} = {sum}");
+
+        // Console.WriteLine("Please input your first name");
+        // string? fname = Console.ReadLine();
+        // Console.WriteLine("Please input your last name");
+        // string? lname = Console.ReadLine();
+        // // PrintNames(fname, lname); 
+        // PrintNames(firstname: fname, lastname: lname);
+
+        // int two_grade_sum = GradeSum(english: 60, math: 90);
+        // Console.WriteLine("Sum Grade of two classes:" + two_grade_sum);
+
+        // int three_grade_sum = GradeSum(english: 60, math: 90, java: 100);
+        // Console.WriteLine($"Sum Grade of three classes: {three_grade_sum}");
+
+        //! Reference
+
+
+
+
+    }
+
+    // static int getInteger()
+    // {
+    //     return Convert.ToInt16(Console.ReadLine());
+    // }
+
+    // static int TwoSum(int a, int b)
+    // {
+    //     int sum = a + b;
+    //     return sum;
+    // }
+
+    // static void PrintNames(string? firstname, string? lastname)
+    // {
+    //     Console.WriteLine($"Hello! {firstname} {lastname} Welcome! to West Texas A&M University!");
+    // }
+
+    // static int GradeSum(int english, int math, int java = 0)
+    // { // java value of 0 is set by default but can be overwritten if passed an argument value for java
+    //     return english + math + java;
+    // }
+
+    // static void refDeposit(ref int accountBalance, int amount)
+    // {
+    //     accountBalance = accountBalance + amount;
+    // }
+
+    // static void refWithDraw(ref int withdrawAmount, int amount)
+    // {
+    //     withdrawAmount = withdrawAmount - amount;
+    // }
+}
