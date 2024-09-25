bb21620c8f9e7cc5092401e763e52fe39cd1249d
TorresjDev <j.torres3.dev@gmail.com>
2024-09-25 01:17:55 -0500

    assignment week 5 complete

--- Classwork/Week5/Program.cs
+++ Classwork/Week5/Program.cs
@@ -1,102 +1,167 @@
 ﻿namespace Week5;
 
+//* SoloLearn C# Intermediate Classes & Objects
+// class Program
+// {
+//     //* SoloLearn C# Intermediate
+//     //? Classes and Objects
+//     class Person
+//     {
+//         int age = 21;
+//         string name = "Billy";
+
+//         public void Greet()
+//         {
+//             Console.WriteLine($"Hello, my name is {name},");
+//             Console.WriteLine($" and I am {age} years old.");
+//         }
+//     }
+
+//     // ? Encapsulation
+//     //  - Encapsulation is the concept of restricting access to certain parts of an object to better protect the data it contains.
+//     class BankAccount
+//     {
+//         private double balance = 0; //private will only be accessed within the class
+//         public void Deposit(double n)
+//         {
+//             balance += n;
+//         }
+
+//         public void Withdraw(double n)
+//         {
+//             balance -= n;
+//         }
+
+//         public double GetBalance()
+//         {
+//             return balance;
+//         }
+//     }
+
+//     // ? Constructors
+//     // - A constructor is a special method that is called when an object is created.
+//     class Person
+//     {
+//         private int age;
+//         private string name;
+//         public Person(string n, int a)
+//         {
+//             age = a;
+//             name = n;
+//         }
+//         public void Greet()
+//         {
+//             Console.WriteLine($"Hello, my name is {name}, and I am {age} years old.");
+//         }
+
+//     }
+
+//     // ? Properties
+//     class Person
+//     {
+//         private int age;
+//         public string Name
+//         {
+//             get
+//             {
+//                 return name;
+//             }
+//             set
+//             {
+//                 name = value;
+//             }
+//         }
+//         // ? auto complete - a shortcut to create a property
+//         // public string Name { get; set; }
+//     }
+//     static void Main(string[] args)
+//     {
+//         //* SoloLearn C# Intermediate
+
+//         // ? Classes and Objects
+//         Person student1 = new Person();
+//         student1.Greet();
+
+//         // ? Encapsulation
+//         BankAccount chase = new BankAccount();
+//         chase.Deposit(160000);
+//         chase.Withdraw(1000);
+//         Console.WriteLine("Balance: $" + chase.GetBalance());
+
+//         // ? Constructors
+//         Person student1 = new Person("Billy", 21);
+//         student1.Greet();
+
+//         // ? Properties
+//         Person student1 = new Person();
+//         student1.Name = "Billy";
+//         Console.WriteLine(student1.Name);
+
+
+//     }
+// }
+
+// * Class Week Lecture
 class Program
 {
-    //* SoloLearn C# Intermediate
-    //? Classes and Objects
-    /*  class Person
-      {
-          int age = 21;
-          string name = "Billy";
-
-          public void Greet()
-          {
-              Console.WriteLine($"Hello, my name is {name},");
-              Console.WriteLine($" and I am {age} years old.");
-          }
-      } */
-
-    // ? Encapsulation
-    //  - Encapsulation is the concept of restricting access to certain parts of an object to better protect the data it contains.
-    /* class BankAccount
-{
-    private double balance = 0; //private will only be accessed within the class
-    public void Deposit(double n)
-    {
-        balance += n;
-    }
-
-    public void Withdraw(double n)
-    {
-        balance -= n;
-    }
-
-    public double GetBalance()
-    {
-        return balance;
-    }
-} */
-
-    // ? Constructors
-    // - A constructor is a special method that is called when an object is created.
-    /* class Person
-     {
-         private int age;
-         private string name;
-         public Person(string n, int a)
-         {
-             age = a;
-             name = n;
-         }
-         public void Greet()
-         {
-             Console.WriteLine($"Hello, my name is {name}, and I am {age} years old.");
-         }
-
-     }*/
-
-    // ? Properties
-    class Person
-    {
-        private int age;
-        public string Name
-        {
-            get
-            {
-                return name;
-            }
-            set
-            {
-                name = value;
-            }
-        }
-        // ? auto complete - a shortcut to create a property
-        // public string Name { get; set; }
-    }
     static void Main(string[] args)
     {
-        //* SoloLearn C# Intermediate
-        /*
-        // ? Classes and Objects
-        // Person student1 = new Person();
-        // student1.Greet();
+        Console.WriteLine("Week 5 nice!");
+        // Console.WriteLine("Please input 2 integers");
+        // int int1 = getInteger();
+        // int int2 = getInteger();
+        // int sum = TwoSum(int1, int2);
+        // Console.WriteLine($"{int1} + {int2} = {sum}");
 
-        // ? Encapsulation
-        // BankAccount chase = new BankAccount();
-        // chase.Deposit(160000);
-        // chase.Withdraw(1000);
-        // Console.WriteLine("Balance: $" + chase.GetBalance());
+        // Console.WriteLine("Please input your first name");
+        // string? fname = Console.ReadLine();
+        // Console.WriteLine("Please input your last name");
+        // string? lname = Console.ReadLine();
+        // // PrintNames(fname, lname); 
+        // PrintNames(firstname: fname, lastname: lname);
 
-        // ? Constructors
-        // Person student1 = new Person("Billy", 21);
-        // student1.Greet();
+        // int two_grade_sum = GradeSum(english: 60, math: 90);
+        // Console.WriteLine("Sum Grade of two classes:" + two_grade_sum);
 
-        // ? Properties
-        // Person student1 = new Person();
-        // student1.Name = "Billy";
-        // Console.WriteLine(student1.Name);
-        */
+        // int three_grade_sum = GradeSum(english: 60, math: 90, java: 100);
+        // Console.WriteLine($"Sum Grade of three classes: {three_grade_sum}");
+
+        //! Reference
+
+
+
 
     }
+
+    // static int getInteger()
+    // {
+    //     return Convert.ToInt16(Console.ReadLine());
+    // }
+
+    // static int TwoSum(int a, int b)
+    // {
+    //     int sum = a + b;
+    //     return sum;
+    // }
+
+    // static void PrintNames(string? firstname, string? lastname)
+    // {
+    //     Console.WriteLine($"Hello! {firstname} {lastname} Welcome! to West Texas A&M University!");
+    // }
+
+    // static int GradeSum(int english, int math, int java = 0)
+    // { // java value of 0 is set by default but can be overwritten if passed an argument value for java
+    //     return english + math + java;
+    // }
+
+    // static void refDeposit(ref int accountBalance, int amount)
+    // {
+    //     accountBalance = accountBalance + amount;
+    // }
+
+    // static void refWithDraw(ref int withdrawAmount, int amount)
+    // {
+    //     withdrawAmount = withdrawAmount - amount;
+    // }
 }
 
--- Homework/Homework5/Program.cs
+++ Homework/Homework5/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Homework 5");
+        // * Q1
+        Console.WriteLine("Q1: Enter 2 numbers, to generate largest number");
+        int numA1 = Convert.ToInt16(Console.ReadLine());
+        int numA2 = Convert.ToInt16(Console.ReadLine());
+        int numALg = findLgNum(numA1, numA2);
+        Console.WriteLine($"The largest number is: {numALg} \n");
+
+        // * Q2
+        Console.WriteLine("Q2: Enter 4 numbers, to generate largest number");
+        int numB1 = Convert.ToInt16(Console.ReadLine());
+        int numB2 = Convert.ToInt16(Console.ReadLine());
+        int numB3 = Convert.ToInt16(Console.ReadLine());
+        int numB4 = Convert.ToInt16(Console.ReadLine());
+        int numBLg = findLgNum(findLgNum(numB1, numB2), findLgNum(numB3, numB4));
+        Console.WriteLine($"The largest number is: {numBLg} \n");
+
+        //  * Q3
+        Console.WriteLine("Q3: Create Account");
+        createAccount();
+    }
+    static int findLgNum(int num1, int num2)
+    {
+        int lgNum = num1 > num2 ? num1 : num2;
+        return lgNum;
+    }
+
+    static bool checkAge(int birth_year)
+    {
+        int age = DateTime.Now.Year - birth_year;
+        return age >= 18;
+    }
+
+    static void createAccount()
+    {
+        Console.WriteLine("Create an Account");
+        Console.WriteLine("Enter your username:");
+        string? username = Console.ReadLine();
+        Console.WriteLine("Enter your password:");
+        string? password = Console.ReadLine();
+        Console.WriteLine("Confirm password \n re-enter your password:");
+        string? password2 = Console.ReadLine();
+        Console.WriteLine("Enter your birth year: ");
+        int birthYr = Convert.ToInt16(Console.ReadLine());
+        bool isOfAge = checkAge(birthYr);
+
+        if (isOfAge)
+        {
+            while (password != password2)
+            {
+                Console.WriteLine("Passwords do not match, please re-enter your passwords");
+                password = Console.ReadLine();
+                password2 = Console.ReadLine();
+            }
+            Console.WriteLine("Account created successfully!");
+            Console.WriteLine($"Welcome {username}");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, must be 18 years or older to create an account...");
+        }
     }
 }
