6f9681a673fad63d39a673ce852e42d2a549ee1c
Jesus Torres <j.torres3.dev@gmail.com>
2024-10-26 02:46:00 -0500

    Week 9 part ii completed

--- Classwork/Week9/Program.cs
+++ Classwork/Week9/Program.cs
@@ -39,17 +39,31 @@ class Program
 
         Console.WriteLine("Week 9 Arrays Part II");
         // 2D array: [,] is the syntax to declare a 2D array, new int[3, 3] is the size of the 2D array 
+        // 2D array is a matrix, arr1 is a 3x3 matrix meaning it has 3 rows and 3 columns
         int[,] two_d_array = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
         int[,] arr1 = { { 1, 2, 3, 4 }, { 4, 5, 6, 8 }, { 7, 8, 9, 1 } };
-
+        Console.WriteLine("2D Array");
         PrintLoopArray(arr1);
 
         // jagged array: is an array of arrays
-
+        Console.WriteLine("Jagged Array");
         int[][] jagged_array = new int[][] {
             new int[] { 1, 2, 3, 4 },
-            new int[] { 5, 6, 7 },
+            new int[] { 5, 6 },
+            new int[] { 7, 8, 9 }
+        };
+
+        int num = jagged_array[2][1];
+
+        Console.WriteLine($"jagged_array[2][1] is {num}");
+        foreach (int[] arr in jagged_array)
+        {
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
         }
 
 
