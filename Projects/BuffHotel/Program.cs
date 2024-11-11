c02ec5dbedfa819f20332c120e8c1546027a9803
Jesus Torres <j.torres3.dev@gmail.com>
2024-11-10 22:40:05 -0600

    final project start up

---
+++ Projects/BuffHotel/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BuffHotel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-----CIDM2315 FINAL PROJECT: JESUS TORRES-----");
+        Console.WriteLine("        ---WELCOME TO BUFF HOTEL---");
+    }
+}
